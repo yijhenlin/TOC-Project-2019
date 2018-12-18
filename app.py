ac5a0a6d7a9a5e1bd6539a825c04e5143a0aefff
yijhenlin <C14046303@mail.ncku.edu.tw>
2018-12-19 02:56:41 +0800

    Simple chatbot success

---
+++ app.py
@@ -0,0 +1,130 @@
+from bottle import route, run, request, abort, static_file
+
+from fsm import TocMachine
+
+
+VERIFY_TOKEN = "1111"
+machine = TocMachine(
+    states=[
+        'user',
+        'save',
+        'load',
+        'delete',
+        'readdate',
+        'readnumber',
+        'check',
+        'getdata',
+        'deletedata'
+    ],
+    transitions=[
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'save',
+            'conditions': 'is_going_to_save'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'load',
+            'conditions': 'is_going_to_load'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'user',
+            'dest': 'delete',
+            'conditions': 'is_going_to_delete'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'save',
+            'dest': 'readdate',
+            'conditions': 'is_going_to_read'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'readdate',
+            'dest': 'readnumber',
+            'conditions': 'is_going_to_read'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'readnumber',
+            'dest': 'check',
+            'conditions': 'is_going_to_check'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'readnumber',
+            'dest': 'save',
+            'conditions': 'is_going_to_retrun'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'load',
+            'dest': 'getdata',
+            'conditions': 'is_going_to_data'
+        },
+        {
+            'trigger': 'advance',
+            'source': 'delete',
+            'dest': 'deletedata',
+            'conditions': 'is_going_to_data'
+        },
+        {
+            'trigger': 'go_to_initial',
+            'source': [
+                'deletedata',
+                'getdata',
+                'check',
+            ],
+            'dest': 'user'
+        },
+        {
+            'trigger': 'advance',
+            'source': '*',
+            'dest': 'user',
+            'conditions': 'is_going_to_user'
+        }
+    ],
+    initial='user',
+    auto_transitions=False,
+    show_conditions=True,
+)
+
+
+@route("/webhook", method="GET")
+def setup_webhook():
+    mode = request.GET.get("hub.mode")
+    token = request.GET.get("hub.verify_token")
+    challenge = request.GET.get("hub.challenge")
+
+    if mode == "subscribe" and token == VERIFY_TOKEN:
+        print("WEBHOOK_VERIFIED")
+        return challenge
+
+    else:
+        abort(403)
+
+
+@route("/webhook", method="POST")
+def webhook_handler():
+    body = request.json
+    print('\nFSM STATE: ' + machine.state)
+    print('REQUEST BODY: ')
+    print(body)
+
+    if body['object'] == "page":
+        event = body['entry'][0]['messaging'][0]
+        machine.advance(event)
+        return 'OK'
+
+
+@route('/show-fsm', methods=['GET'])
+def show_fsm():
+    machine.get_graph().draw('fsm.png', prog='dot', format='png')
+    return static_file('fsm.png', root='./', mimetype='image/png')
+
+
+if __name__ == "__main__":
+    run(host="localhost", port=5000, debug=True, reloader=True)
---
+++ fsm.py
@@ -0,0 +1,105 @@
+from transitions.extensions import GraphMachine
+
+from utils import send_text_message
+
+money = {}
+date = ''
+number = ''
+class TocMachine(GraphMachine):
+    def __init__(self, **machine_configs):
+        self.machine = GraphMachine(
+            model=self,
+            **machine_configs
+        )
+    def is_going_to_user(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() == '..'
+        return False
+    def is_going_to_delete(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() == '刪除'
+        return False
+
+    def is_going_to_save(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() == '存檔'
+        return False
+    def is_going_to_load(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() == '讀檔'
+        return False
+    def is_going_to_read(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() != ''
+        return False
+    def is_going_to_check(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() == '確認'
+        return False
+    def is_going_to_retrun(self, event):
+        if event.get("message"):
+            text = event['message']['text']
+            return text.lower() != '確認'
+        return False
+    def is_going_to_data(self, event):
+        global date
+        if event.get("message"):
+            sender_id = event['sender']['id']
+            date = event['message']['text']
+            flag = str(date) in money
+            if(date != '..' and flag == False):
+                send_text_message(sender_id, "無此輸入資料\n請輸入日期(MM/DD)")
+            return flag
+        return False
+    def on_enter_user(self, event):
+        sender_id = event['sender']['id']
+        send_text_message(sender_id, "請選擇存檔或讀檔或刪除")
+    def on_enter_save(self, event):
+        sender_id = event['sender']['id']
+        send_text_message(sender_id, "請輸入日期(MM/DD)")
+    def on_enter_readdate(self, event):
+        global date
+        sender_id = event['sender']['id']
+        date = event['message']['text']
+        send_text_message(sender_id, "請輸入支出金額")
+    def on_enter_readnumber(self, event):
+        global number
+        sender_id = event['sender']['id']
+        number = event['message']['text']
+        temp = "%s 支出 %s\n請確認資料無誤"%(date,number)
+        send_text_message(sender_id, temp)
+    def on_enter_check(self, event):
+        sender_id = event['sender']['id']
+        money[date] = number
+        send_text_message(sender_id, "存檔成功")
+        send_text_message(sender_id, "請選擇存檔或讀檔或刪除")
+        self.go_to_initial()
+    def on_enter_load(self, event):
+        sender_id = event['sender']['id']
+        temp = "目前紀錄 %s"%(money.keys())
+        send_text_message(sender_id, temp)
+        send_text_message(sender_id, "請輸入日期(MM/DD)")
+    def on_enter_getdata(self, event):
+        sender_id = event['sender']['id']
+        temp = "%s 支出 %s"%(date,money[date])
+        send_text_message(sender_id, temp)
+        send_text_message(sender_id, "請選擇存檔或讀檔或刪除")
+        self.go_to_initial()
+    def on_enter_delete(self, event):
+        sender_id = event['sender']['id']
+        temp = "目前紀錄 %s"%(money.keys())
+        send_text_message(sender_id, temp)
+        send_text_message(sender_id, "請輸入日期(MM/DD)")
+    def on_enter_deletedata(self, event):
+        sender_id = event['sender']['id']
+        del money[date]
+        send_text_message(sender_id, "刪除成功")
+        send_text_message(sender_id, "請選擇存檔或讀檔或刪除")
+        self.go_to_initial()
+
